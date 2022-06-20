1031a678047c207293e905efc0ba89f6db4acd4d
Mike Kroutikov <mkroutikov@innodata.com>
2022-06-20 17:49:04 -0400

    use wekref machinery to ensure correct destruction order; PDFFile.load now can be used as a context manager

--- pdfbrain/pdf_file.py
+++ pdfbrain/pdf_file.py
@@ -1,14 +1,23 @@
-import pypdfium as pdfium
+import pypdfium2 as pdfium
+import sys
 from .tools import lazyproperty, get_error_message
 from .pdf_page import PDFPage
+import weakref
 
 # this line is very important, otherwise it won't work
 pdfium.FPDF_InitLibraryWithConfig(pdfium.FPDF_LIBRARY_CONFIG(2, None, None, 0))
 
 
+def _close_file(fileref):
+    file = fileref()
+    if file:
+        file.close()
+
 class PDFFile:
-    def __init__(self, ptr):
+    def __init__(self, ptr, filename):
         self._ptr = ptr
+        self.filename = filename
+        weakref.finalize(self, _close_file, weakref.ref(self))
 
     @classmethod
     def load(cls, filename, password=None):
@@ -16,10 +25,18 @@ class PDFFile:
         if not ptr:
             err_message = get_error_message()
             raise RuntimeError(f'PDFFile.load: failed to load {filename}: {err_message}')
-        return cls(ptr)
+        return cls(ptr, filename)
 
-    def __del__(self):
-        pdfium.FPDF_CloseDocument(self._ptr)
+    def close(self):
+        if self._ptr:
+            pdfium.FPDF_CloseDocument(self._ptr)
+            self._ptr = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        self.close()
 
     @lazyproperty
     def numpages(self):
--- pdfbrain/test/test_smoke.py
+++ pdfbrain/test/test_smoke.py
@@ -16,6 +16,20 @@ def test_smoke():
     img = pdf[1].render()
     assert img.size == (612, 792)
 
+def test_with():
+    with PDFFile.load(res('simple.pdf')) as pdf:
+        assert len(pdf) == 2
+
+        page0 = pdf[0]
+        assert page0.label == ''
+        assert page0.media_box == (0.0, 0.0, 612.0, 792.0)
+        assert page0.crop_box == (0.0, 0.0, 612.0, 792.0)
+
+    pdf.close()  # should be NOOP
+    pdf.close()  # should be NOOP
+    pdf.close()  # should be NOOP
+
+
 def test_text():
     pagen0 = PDFFile.load(res('simple.pdf'))[0]
 
